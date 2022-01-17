17eb1e0a622f4a7ad4255ef1b593d76692ca0069
Park Sangyeop <meko6970@gmail.com>
2022-01-18 00:10:50 +0900

    220117 /정렬 / 11650 / 좌표 정렬하기

--- sort/lev6.py
+++ sort/lev6.py
@@ -1,12 +1,14 @@
 # 220117 /정렬 / 11650 / 좌표 정렬하기 
-
-N = int(input())
+import sys
+N = int(sys.stdin.readline())
 
 loc1 = []
 
 for i in range(N):
-    [a,b] = map(int, input().split())
+    [a,b] = map(int, sys.stdin.readline().split())
     loc1.append([a, b])
 
+loc = sorted(loc1, key=lambda x:(x[0], x[1]))
+
 for i in range(N):
-    print(loc1[i][0], loc1[i][1])
+    print(loc[i][0], loc[i][1])
