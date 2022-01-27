99aebb4e14b8ddfe1a5f839bb28aa99e70d1d67e
Park Sangyeop <meko6970@gmail.com>
2022-01-27 23:54:47 +0900

    # 220127 / 동적계획법1 / 1904 / 01타일

---
+++ dynamic/lev3.py
@@ -0,0 +1,15 @@
+# 220127 / 동적계획법1 / 1904 / 01타일
+
+import sys 
+input = sys.stdin.readline 
+n = int(input()) 
+
+dp = [0] * 1000001 
+dp[1] = 1 
+dp[2] = 2 
+
+for k in range(3,n+1): 
+    dp[k] = (dp[k-1]+ dp[k-2])%15746 
+
+print(dp[n])
+
