0973d356b1d84edc87c2a9e6871eddb46e6bf3ad
Park Sangyeop <meko6970@gmail.com>
2022-02-02 00:24:54 +0900

    220201 / 동적계획법1 / 11053 / 가장 긴 증가하는 부분 수열2

--- dynamic/lev11 긴증가하는 부분수열.py
+++ dynamic/lev11 긴증가하는 부분수열.py
@@ -3,31 +3,13 @@
 n = int(input())
 arr = list(map(int,input().split()))
 dp = [0]*n
-print(arr[:3])
 
-dp[0] = 1
-for i in range(1, n):
-    j = arr[i]
-    while True:
-        j -= 1
-        if j in arr[:i]:
-            b = arr[:i]
-            a = b[::-1].index(j)
-            dp[i] = dp[i-a] + 1
-            break
-        
-        if j <= min(arr):
-            dp[i] = 1
-            break
+
+for i in range(n):
+    for j in range(i):
+        if arr[i] > arr[j] and dp[i] < dp[j]:
+            dp[i] = dp[j]
+    dp[i]+=1
 
 print(max(dp))
-print(dp)
 
-"""
-2 7
-7-2+1
-0 2 7
-7-2
-1 1
-2 2번째 이전 값 비교
-"""
