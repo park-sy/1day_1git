da7ad4c05b9ddaa343a880c925a91808f486f568
Park Sangyeop <meko6970@gmail.com>
2022-02-03 21:58:35 +0900

    220203 / 동적계획법 1 / 9251 / LCS

---
+++ dynamic/lev14 LCS.py
@@ -0,0 +1,15 @@
+# 220203 / 동적계획법 1 / 9251 / LCS
+import sys
+A = ' ' + sys.stdin.readline().rstrip()
+B = ' ' + sys.stdin.readline().rstrip()
+dp = [[0 for _ in range(len(B))] for _ in range(len(A))]
+
+
+for i in range(1,len(A)):
+    for j in range(1,len(B)):
+        if A[i] == B[j]:
+            dp[i][j] = dp[i-1][j-1] + 1
+        else:
+            dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+
+print(dp[-1][-1])
