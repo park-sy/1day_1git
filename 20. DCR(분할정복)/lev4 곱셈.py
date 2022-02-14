f01c74d72a6a1e2950cf485ff7d81e4f6c82b186
Park Sangyeop <meko6970@gmail.com>
2022-02-14 23:21:49 +0900

    220214 / 분할정복 / 1629 / 곱셈/실패

---
+++ 20. DCR(분할정복)/lev4 곱셈.py
@@ -0,0 +1,19 @@
+# 220214 / 분할정복 / 1629 / 곱셈
+
+A,B,C = list(map(int,input().split()))
+
+
+def power(a, b):
+    if b == 1: # b의 값이 1이면 a % C를 return한다.
+        return a % C
+    else:
+        temp = power(a, b // 2) # a^(b // 2)를 미리 구한다.
+        if b % 2 == 0:
+            return temp * temp % C # b가 짝수인 경우
+        else:
+            return temp * temp * a % C # b가 홀수인 경우
+
+
+
+result = power(A, B)
+print(result)
