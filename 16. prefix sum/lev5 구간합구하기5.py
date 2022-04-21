fe22ef46dd0ca38b545a3a2a33ef3e74befc4244
Park Sangyeop <meko6970@gmail.com>
2022-04-21 23:56:43 +0900

    220421 / 누적합 / 11660 / 구간 합 구하기 5

--- 16. prefix sum/lev5 구간합구하기5.py
+++ 16. prefix sum/lev5 구간합구하기5.py
@@ -14,4 +14,7 @@ for i in range(1,n+1):
 
 for _ in range(m):
     x1,y1,x2,y2 = map(int,input().split())
-    print(dp[x1][y2]-dp[x1][y1-1] + dp[x2][y2] - dp[x2][y1-1])
+    sum = 0
+    for i in range(x1,x2+1):
+        sum += dp[i][y2] - dp[i][y1-1]
+    print(sum)
