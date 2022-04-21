72630ece74d7ef0175e66608c64261f23e07c09d
Park Sangyeop <meko6970@gmail.com>
2022-04-21 23:52:24 +0900

    220421 / 누적합 / 11660 / 구간 합 구하기 5

---
+++ 16. prefix sum/lev5 구간합구하기5.py
@@ -0,0 +1,17 @@
+# 220421 / 누적합 / 11660 / 구간 합 구하기 5
+import sys
+input = sys.stdin.readline
+
+n, m = map(int,input().split())
+nums = [[]]
+for _ in range(n):
+    nums.append([0]+list(map(int,input().split())))
+
+dp = [[0]*(n+1)for _ in range(n+1)]
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        dp[i][j] = dp[i][j-1] + nums[i][j]
+
+for _ in range(m):
+    x1,y1,x2,y2 = map(int,input().split())
+    print(dp[x1][y2]-dp[x1][y1-1] + dp[x2][y2] - dp[x2][y1-1])
