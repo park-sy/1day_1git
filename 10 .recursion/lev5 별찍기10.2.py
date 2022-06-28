e0f92e1414310fff9dea05aa2c08df2b274d87e2
Park Sangyeop <meko6970@gmail.com>
2022-06-28 23:50:59 +0900

    220628 / recursion / 2447 / 별찍기 - 10

---
+++ 10 .recursion/lev5 별찍기10.2.py
@@ -0,0 +1,17 @@
+# 220628 / recursion / 2447 / 별찍기 - 10
+
+def append_star(LEN): 
+    if LEN == 1: 
+        return ['*'] 
+    Stars = append_star(LEN//3) 
+    L = []
+    for S in Stars: 
+        L.append(S*3) 
+    for S in Stars: 
+        L.append(S+' '*(LEN//3)+S) 
+    for S in Stars: 
+        L.append(S*3) 
+    return L 
+        
+n = int(input()) 
+print('\n'.join(append_star(n)))
