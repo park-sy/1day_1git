b64aa45bdb0041c088fd0de40f97ad479d5c79b0
Park Sangyeop <meko6970@gmail.com>
2022-08-21 23:51:28 +0900

    220821 / 사회망 서비스

---
+++ BOJ/31. dp in tree/lev3 사회망 서비스.py
@@ -0,0 +1,25 @@
+# 220821 / 사회망 서비스
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+graph = [[]for _ in range(n+1)]
+for _ in range(n-1):
+    n1,n2 = map(int,input().split())
+    graph[n1].append(n2)
+    graph[n2].append(n1)
+
+visit = [False]*(n+1)
+dp =[[0,0]for _ in range(n+1)]
+
+def dfs(start):
+    visit[start] = True
+    dp[start][0] = 1
+    for i in graph[start]:
+        if not visit[i]:
+            dfs(i)
+            dp[start][0] += min(dp[i][0],dp[i][1])
+            dp[start][1] += dp[i][0]
+
+dfs(1)
+print(min(dp[1][0],dp[1][1]))
